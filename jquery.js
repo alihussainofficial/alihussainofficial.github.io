63a1f8385b42f83b59e1ad50803363ef5dedde24
alihussainofficial <76662174+alihussainofficial@users.noreply.github.com>
2020-12-28 00:36:05 +0500

    added new files

---
+++ jquery.js
@@ -0,0 +1,10 @@
+  <!--JQUery-------------------->
+<script type="text/javascript" src="js/JQuery.js"></script>
+  
+	$(document).ready(function(){
+		$('.toggle').click(function(){
+			$('.toggle').toggleClass('active')
+			$('nav').toggleClass('active')
+		})
+	})
+	</script>
